f99d19071b4bdd8f0e2004d687f067979aa2e12e
lzfresh001 <214222764@qq.com>
2019-05-27 14:42:18 +0800

    提交项目

---
+++ gittest/src/com/lzf/git/GitTest.java
@@ -0,0 +1,20 @@
+package com.lzf.git;
+/**@文件名: GitTest.java
+ * @类功能说明: 
+ * @作者: Z.fresh Lee
+ * @Email: 214222764@qq.com
+ * @日期: 2019年5月27日下午2:17:38
+ * @修改说明:<br> 
+ * <pre>
+ * 	 <li>作者: Z.fresh Lee</li> 
+ * 	 <li>日期: 2019年5月27日下午2:17:38</li> 
+ *	 <li>内容: </li>
+ * </pre>
+ */
+public class GitTest {
+	
+	public static void main(String[] args) {
+		System.out.println("hello Git");
+	}
+
+}
